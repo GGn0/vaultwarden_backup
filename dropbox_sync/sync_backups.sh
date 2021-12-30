5271fdaaa108a7ad2358f60627d8388ef5666318
Luigi Scalzone <44038661+GGn0@users.noreply.github.com>
2021-12-30 20:37:51 +0100

    Update sync_backups.sh
    Modify the script to be run by root

--- dropbox_sync/sync_backups.sh
+++ dropbox_sync/sync_backups.sh
@@ -11,16 +11,17 @@ source ./CONFIG
 
 unset LOCAL_LIST
 unset LOCAL_ARRAY
-REMOTE_LIST=$(./"${UPLOADER}" list)
+sudo -u ${USR} REMOTE_LIST=$(./"${UPLOADER}" list ${REMOTEDIR})
 LOCAL_LIST+=$(ls -1r "${SOURCEDIR}")
 IFS=$'\n' read -rd '' -a LOCAL_ARRAY <<<"${LOCAL_LIST[@]}"; unset IFS
 
 TMPDIR=$(mktemp -d)
-
+chown ${USR}:${USR} "${TMPDIR}"
 for i in ${!LOCAL_ARRAY[@]}; do
-	printf -v FILENAME "%03d.tar.gz" $i
-	openssl enc -aes256 -in "${SOURCEDIR}/${LOCAL_ARRAY[$i]}" -pbkdf2 -iter 100000 -pass file:"${PWD}" -out "${TMPDIR}/${FILENAME}"
-	./"${UPLOADER}" upload "${TMPDIR}/${FILENAME}" "${REMOTE_DIR}/"
+        printf -v FILENAME "%03d.tar.gz" $i
+        openssl enc -aes256 -in "${SOURCEDIR}/${LOCAL_ARRAY[$i]}" -pbkdf2 -iter 100000 -pass file:"${PWD}" -out "${TMPDIR}/${FILENAME}"
+        chown ${USR} "${TMPDIR}/${FILENAME}"
+        sudo -u ${USR} ./"${UPLOADER}" upload "${TMPDIR}/${FILENAME}" "${REMOTE_DIR}/"
 done
 
 rm -rf "${TMPDIR}"
